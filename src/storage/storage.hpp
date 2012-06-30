28fb8c25d9cce922f13e79e206ed859fcb61915b
Wouter Geraedts <w.geraedts@onii.org>
2012-06-30 15:10:45 +0200

    Removed duplicate librusql files Changed storage to use librusql

--- src/storage/storage.hpp
+++ src/storage/storage.hpp
@@ -1,17 +1,17 @@
 #include <memory>
 
-#include "connection.hpp"
+#include <rusql/connection.hpp>
 
 namespace ruuster
 {
 	class storage
 	{
 	private:
-		std::shared_ptr<mysql::connection> conn;
+		std::shared_ptr<rusql::connection> conn;
 		
 	public:
 		storage(const std::string host, const std::string user, const std::string password, const std::string database)
-		: conn(std::make_shared<mysql::connection>(mysql::connection::connection_info(host, user, password, database)))
+		: conn(std::make_shared<rusql::connection>(rusql::connection::connection_info(host, user, password, database)))
 		{}
 	};
 }
